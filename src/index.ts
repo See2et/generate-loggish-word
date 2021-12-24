651e32010743a70c1a81a807267551faad763414
Seezet <Seezet1024@gmail.com>
2021-12-24 16:49:39 +0900

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import loggish from '../loggish.json'
+import loggish from '../loggish.json';
+import * as fs from 'fs';
+
+const words = {};
 
 const v = loggish.vowel;
 const c = loggish.consonant;
@@ -10,9 +13,10 @@ let c1: number = 0;
 let v2: number = 0;
 let c2: number = 0;
 let i: number = 0;
+let n: number = 0;
 
 while (true) {
-    i += 2;
+    n += 2;
     v1++;
     if (v1 > vl) {
         v1 = v1 % vl;
@@ -25,8 +29,10 @@ while (true) {
     if (c2 >= cl) {
         break;
     }
-    const word = loggish.consonant[c2 % cl] + loggish.vowel[i % vl] + loggish.consonant[c1 % cl] + loggish.vowel[v1 % vl];
-    console.log(word);
+    const word = loggish.consonant[c2 % cl] + loggish.vowel[n % vl] + loggish.consonant[c1 % cl] + loggish.vowel[v1 % vl];
+    // console.log(word);
+    words[i] = word;
+    i++;
 }
 
 v1 = 0;
@@ -35,10 +41,10 @@ v2 = 0;
 c2 = 0;
 let v3: number = 0;
 let c3: number = 0;
-i = 0;
+n = 0;
 
 while (true) {
-    i += 2;
+    n += 2;
     v1++;
     if (v1 > vl) {
         v1 = v1 % vl;
@@ -59,6 +65,15 @@ while (true) {
     if (c3 >= cl) {
         break;
     }
-    const word = c[c3 % cl] + v[i % vl] + c[c2 % cl] + v[v2 % vl] + c[c1 % cl] + v[v1 % vl];
-    console.log(word);
+    const word = c[c3 % cl] + v[n % vl] + c[c2 % cl] + v[v2 % vl] + c[c1 % cl] + v[v1 % vl];
+    // console.log(word);
+    words[i] = word;
+    i++;
 }
+
+const result = {};
+Object.keys(words).forEach(function(key){
+    result[key] = words[key];
+    //console.log(key,obj[key])
+});
+fs.writeFileSync('output.json', JSON.stringify(result));
