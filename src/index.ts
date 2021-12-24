b15541fa367cdc8e68d294fe5dba4b7adca7c178
Seezet <Seezet1024@gmail.com>
2021-12-24 15:04:31 +0900

    change loggish.json

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import loggish from '../loggish.json'
 
-const vl = loggish.vowel.length;
-const cl = loggish.consonant.length;
+const v = loggish.vowel;
+const c = loggish.consonant;
+const vl = v.length;
+const cl = c.length;
 
 let v1: number = 0;
 let c1: number = 0;
@@ -24,5 +26,39 @@ while (true) {
         break;
     }
     const word = loggish.consonant[c2 % cl] + loggish.vowel[i % vl] + loggish.consonant[c1 % cl] + loggish.vowel[v1 % vl];
+    console.log(word);
+}
+
+v1 = 0;
+c1 = 0;
+v2 = 0;
+c2 = 0;
+let v3: number = 0;
+let c3: number = 0;
+i = 0;
+
+while (true) {
+    i += 2;
+    v1++;
+    if (v1 > vl) {
+        v1 = v1 % vl;
+        c1++;
+    }
+    if (c1 > cl) {
+        c1 = c1 % cl;
+        v2++;
+    }
+    if (v2 > vl) {
+        v2 = v2 % vl;
+        c2++;
+    }
+    if (c2 > cl) {
+        c2 = c2 % cl;
+        c3++;
+    }
+    if (c3 >= cl) {
+        break;
+    }
+    const word = c[c3 % cl] + v[i % vl] + c[c2 % cl] + v[v2 % vl] + c[c1 % cl] + v[v1 % vl];
     console.log(word);
 }
